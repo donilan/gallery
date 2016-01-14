2579f04ec78e091668382d4e1c9e4c96ccab69ad
Doni Leung <doni@kudelabs.com>
2016-01-14 17:07:36 +0800

    Load more photos when scroll to bottom.

--- react/gallery.js
+++ react/gallery.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Gallery from 'react-photo-gallery';
 import Lightbox from 'react-images';
 import Loading from './loading';
+import _ from 'lodash';
 
 const lightboxStyles  = Lightbox.extendStyles({
   backdrop: {
@@ -15,12 +17,50 @@ const lightboxStyles  = Lightbox.extendStyles({
 
 module.exports = React.createClass({
   getInitialState: function() {
-    return {photos: null};
+    return {
+      photos: null,
+      maxPhotos: 0,
+      containerWidth: 0,
+      loadSize: 0
+    };
   },
   componentWillMount: function() {
+    window.addEventListener('scroll', this.handleScroll);
     $.get('/images', function(images){
       this.setState({photos: images});
     }.bind(this));
+  },
+  calcState: function(containerWidth) {
+    if (containerWidth >= 1024){
+      return {loadSize: 3, containerWidth: containerWidth, maxPhotos: 9};
+    } else if (containerWidth >= 480){
+      return {loadSize: 2, containerWidth: containerWidth, maxPhotos: 6};
+    } else {
+      return {loadSize: 1, containerWidth: containerWidth, maxPhotos: 2};
+    }
+  },
+  componentDidMount: function(){
+    var containerWidth = Math.floor(ReactDOM.findDOMNode(this).clientWidth);
+    this.setState(this.calcState(containerWidth));
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  handleResize: function(e){
+    var containerWidth = ReactDOM.findDOMNode(this).clientWidth;
+    if (ReactDOM.findDOMNode(this).clientWidth !== this.state.containerWidth){
+      console.log(this.calcState(containerWidth));
+      this.setState(this.calcState(containerWidth));
+    }
+  },
+  handleScroll: function(e) {
+    if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 50)) {
+      this.loadMorePhotos();
+    }
+  },
+  loadMorePhotos: function() {
+    if(this.state.photos && this.state.photos.length > this.state.maxPhotos) {
+      this.setState({maxPhotos: this.state.maxPhotos + this.state.loadSize});
+    }
   },
   render: function() {
     if(!this.state.photos) {
@@ -28,7 +68,7 @@ module.exports = React.createClass({
     }
     return (
       <div>
-        <Gallery photos={this.state.photos} lightboxStyles={lightboxStyles} />
+        <Gallery photos={_.take(this.state.photos, this.state.maxPhotos)} lightboxStyles={lightboxStyles} />
       </div>
     );
   }
