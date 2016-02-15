9073d1931e8d08d4815b88ea0ad0c3eeefa84305
Doni Leung <doni@kudelabs.com>
2016-02-15 20:35:37 +0800

    Enabled cache.

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,10 +1,10 @@
 class MediasController < ApplicationController
   include MediasHelper
-  THUMB_WIDTH = 400
+  THUMB_WIDTH = 1300
   def index
-    # if !File.exist?(media_cache_path)
-    #   FileUtils.mkdir_p(media_cache_path)
-    # end
+    if !File.exist?(media_cache_path)
+      FileUtils.mkdir_p(media_cache_path)
+    end
     medias = Rails.cache.fetch('medias-controller-index', expires_in: 60.seconds) do
       Dir[media_path.join('*').to_s].sort_by{ |f| File.mtime(f) }.reverse.map do |path|
         handle_name = "handle_#{File.extname(path)[1..-1].downcase}".to_sym
@@ -38,20 +38,20 @@ class MediasController < ApplicationController
 
   def handle_jpg(path)
     name = File.basename path
-    # cache_path = media_cache_path.join(name).to_s
-    # if !File.exist?(cache_path)
-    #   image = MiniMagick::Image.open(path)
-    #   width = image.width
-    #   while width > THUMB_WIDTH
-    #     width /= 1.4
-    #   end
-    #   image.resize("1000>x#{width}>")
-    #   image.write(cache_path)
-    # end
-    meta = MiniMagick::Image.open(path)
+    cache_path = media_cache_path.join(name).to_s
+    if !File.exist?(cache_path)
+      image = MiniMagick::Image.open(path)
+      # width = image.width
+      # while width > THUMB_WIDTH
+      #   width /= 1.4
+      # end
+      image.resize("1000>x#{THUMB_WIDTH}>")
+      image.write(cache_path)
+    end
+    meta = MiniMagick::Image.open(cache_path)
     {
       type: 'image',
-      src: media_uri(name),
+      src: media_cache_uri(name),
       width: meta.width,
       height: meta.height,
       aspectRatio: (1.0 * meta.width / meta.height).round(2),
